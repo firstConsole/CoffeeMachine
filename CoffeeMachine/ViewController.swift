8d189fa71561825f74f27078b1187f2a8e19f5ca
Артамонов Алексей Викторович <94192633+firstConsole@users.noreply.github.com>
2022-12-25 18:24:07 +0300

    Initial Commit

---
+++ CoffeeMachine/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoffeeMachine
+//
+//  Created by Алексей Артамонов on 25.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
